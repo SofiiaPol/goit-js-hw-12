5f82d3e4c2e5f23458d1dd37287043b625252378
SofiiaPol <sophia.polyakova05@gmail.com>
2024-05-19 22:19:04 +0300

    change html and add function for load button

--- src/main.js
+++ src/main.js
@@ -9,12 +9,16 @@ import { renderImages } from './js/render-functions';
 
 let page = 1;
 const PER_PAGE = 15;
-const images = [];
+let images = [];
+let query = '';
+let total = 0;
 
 const searchForm = document.querySelector('.search-form');
+const loadMoreButton = document.querySelector('.load-button');
 const loader = document.querySelector('.loader');
 
 let lightbox = new SimpleLightbox('.gallery a');
+updateLoadButton('none');
 
 const onFormSubmit = async event => {
   event.preventDefault();
@@ -26,8 +30,11 @@ const onFormSubmit = async event => {
     return;
   }
   try {
+    page = 1;
     const data = await fetchImages(userInput);
     updateLoader('none');
+    updateLoadButton('block');
+
     if (data.totalHits === 0) {
       showMessage(
         `Sorry, there are no images matching your search query. Please try again!`
@@ -35,6 +42,30 @@ const onFormSubmit = async event => {
     }
     renderImages(data.hits);
     lightbox.refresh();
+    query = userInput;
+    total = data.totalHits;
+    images = data.hits;
+  } catch (error) {
+    updateLoader('none');
+    showError(error.message);
+  }
+};
+
+const onLoadMoreButtonClick = async event => {
+  event.preventDefault();
+  try {
+    page = page + 1;
+    if (images.length >= total) {
+      updateLoadButton('none');
+      showMessage(`We're sorry, but you've reached the end of search results.`);
+      return;
+    }
+    const data = await fetchImages(query, page, PER_PAGE);
+    updateLoader('none');
+    const result = [...images, ...data.hits];
+    renderImages(result);
+    lightbox.refresh();
+    images = result;
   } catch (error) {
     updateLoader('none');
     showError(error.message);
@@ -42,6 +73,7 @@ const onFormSubmit = async event => {
 };
 
 searchForm.addEventListener('submit', onFormSubmit);
+loadMoreButton.addEventListener('click', onLoadMoreButtonClick);
 
 function showError(errorMessage) {
   iziToast.show({
@@ -64,18 +96,7 @@ function showMessage(message) {
 function updateLoader(value) {
   loader.style.display = value;
 }
-// fetchImages(userInput)
-//   .then(data => {
-//     updateLoader('none');
-//     if (data.totalHits === 0) {
-//       showMessage(
-//         `Sorry, there are no images matching your search query. Please try again!`
-//       );
-//     }
-//     renderImages(data.hits);
-//     lightbox.refresh();
-//   })
-//   .catch(error => {
-//     updateLoader('none');
-//     showError(error.message);
-//   });
+
+function updateLoadButton(value) {
+  loadMoreButton.style.display = value;
+}
